5fcd2d7262334767bfc003fbe661ac3ede11c6a4
Phil Freeman <paf31@cantab.net>
2016-09-24 14:45:26 -0700

    It is

---
+++ verify-package-set/src/Verify.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Verify
+  ( decodePackagesSpec
+  , verifyPackageSet
+  ) where
+
+import Control.Arrow ((&&&))
+import Control.Lens ((<.), toListOf, to, ifolded, withIndex)
+import Control.Monad (when)
+import Data.Aeson (FromJSON, ToJSON, decodeStrict)
+import Data.ByteString (ByteString)
+import Data.Foldable (for_)
+import Data.Map (Map, toList)
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8')
+import Filesystem.Path ((</>))
+import Filesystem.Path.CurrentOS (fromText, toText)
+import GHC.Generics (Generic)
+import Turtle
+
+type PackagesSpec = Map PackageName PackageSpec
+
+type Repo = Text
+
+type Version = Text
+
+type PackageName = Text
+
+data PackageSpec = PackageSpec
+  { repo         :: Repo
+  , version      :: Version
+  , dependencies :: [PackageName]
+  } deriving (Show, Eq, Generic, FromJSON, ToJSON)
+
+decodePackagesSpec :: ByteString -> Maybe PackagesSpec
+decodePackagesSpec = decodeStrict
+
+getGitRepoList :: PackagesSpec -> [(PackageName, (Repo, Version))]
+getGitRepoList = toListOf ((ifolded <. to (repo &&& version)) . withIndex)
+
+verifyPackageSet :: PackagesSpec -> IO ()
+verifyPackageSet ps = sh $ do
+    -- Wipe out the current packages directory
+    rmdirIfExists "packages"
+
+    -- Clone all repos into the packages/ directory
+    for_ (getGitRepoList ps) $ \(name, (repo, version)) -> do
+      procs "git"
+            [ "clone"
+            , repo
+            , "-b", version
+            , explode $ toText ("packages" </> fromText name)
+            ] empty
+
+    -- Print out the psc version
+    procs "psc" [ "--version" ] empty
+
+    for_ (toList ps) $ \(name, PackageSpec{..}) -> do
+      echo ("Building package " <> name)
+      rmdirIfExists "output"
+      let srcGlobs = map (("packages/" <>) . (<> "/src/**/*.purs")) (name : dependencies)
+      procs "psc"
+            srcGlobs
+            empty
+
+  where
+    explode = either (error . show) id
+
+    rmdirIfExists dir = do
+      exists <- testdir dir
+      when exists (rmtree dir)
