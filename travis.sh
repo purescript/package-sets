c33733ac2c25e6a70f8dc33bfff92ae619ede4da
Phil Freeman <paf31@cantab.net>
2017-01-05 20:05:51 -0800

    Use the current branch, fix #43 (#44)
    * Use the current branch, fix #43
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Use a script

---
+++ travis.sh
@@ -0,0 +1,4 @@
+echo '{ "name": "", "set": "testing", "source": "", "depends": [] }' > psc-package.json
+mkdir -p .psc-package/testing/.set
+cp packages.json .psc-package/testing/.set/packages.json
+psc-package verify-set
