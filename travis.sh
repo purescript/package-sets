9f7583a08d4c29179a71fc5a7839b3aba436bdd1
Daniel Gasienica <daniel@gasienica.ch>
2018-12-24 11:10:39 -0600

    Allow selecting which package to verify
    This simplifies these instructions:
    https://github.com/purescript/psc-package/pull/145

--- travis.sh
+++ travis.sh
@@ -1,4 +1,6 @@
+PACKAGE="${1:-}"
+
 echo '{ "name": "test-package", "set": "testing", "source": "", "depends": [] }' > psc-package.json
 mkdir -p .psc-package/testing/.set
 cp packages.json .psc-package/testing/.set/packages.json
-psc-package verify
+psc-package verify $PACKAGE
