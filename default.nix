f1ff1c73adaea7525b1a439d0cb23a5e9d692da6
Justin Woo <moomoowoo@gmail.com>
2019-07-15 01:47:10 +0300

    Generate packages dhall release (#402)
    * generate packages.dhall and push to release on tags
    
    * update default.nix

--- default.nix
+++ default.nix
@@ -4,9 +4,11 @@ let
   easy-ps = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "f1e038e20596512656413fd3c192f57ad4fc88d3";
-    sha256 = "1lxi09fnlgq4bcbcdjml1lvcldkh0kaz8x09ayw2s8dz8pzkv7a6";
-  });
+    rev = "9a8d138663c5d751e3a84f1345166e1f0f760a07";
+    sha256 = "1c0mqn4wxh4bmxnf6hgrhk442kl2m9y315wik87wrw2ikb7s1szf";
+  }) {
+    inherit pkgs;
+  };
 
 in pkgs.runCommand "easy-ps-test" {
   buildInputs = builtins.attrValues {
