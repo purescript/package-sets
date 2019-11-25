c5df7e707456b887a7bf5560aacee4d68d21ad76
Justin Woo <moomoowoo@gmail.com>
2019-11-25 22:29:46 +0200

    update to 0.13.5 (#541)

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@ let
     pkgs.fetchFromGitHub {
       owner = "justinwoo";
       repo = "easy-purescript-nix";
-      rev = "b2efbe30c55ffa16dd6f85cb7c71f77ac3136aa0";
-      sha256 = "0mmjvqpkns964sjkbw0waqb47vfhg9r0fp9y0b5pizpikmw3fbp2";
+      rev = "6be3f48f339034a58b1b1ae997ace534cf459826";
+      sha256 = "10fxfxgbpr920bj69jail8vsj6qj5cf4g2r5brxiv23fz8nkzf5n";
     }
   ) {
     inherit pkgs;
@@ -15,6 +15,6 @@ let
 in
 pkgs.runCommand "easy-ps-test" {
   buildInputs = builtins.attrValues {
-    inherit (easy-ps) purs-0_13_4 psc-package dhall-simple dhall-json-simple;
+    inherit (easy-ps) purs-0_13_5 psc-package dhall-simple dhall-json-simple;
   } ++ [ pkgs.git ];
 } ""
