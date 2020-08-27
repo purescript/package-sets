f238688dab463aa6ac3d3a03bbbfd466280cfec2
andys8 <andys8@users.noreply.github.com>
2020-08-27 12:19:28 +0200

    [Improvement] verify packages in registry script
    * Cleanup downloaded files
    * Hide wget output
    * End with success message

--- verify-packages-in-registry.sh
+++ verify-packages-in-registry.sh
@@ -5,16 +5,18 @@ set -euo pipefail
 
 # This script checks that all the packages in the set are present in the registry
 
-wget https://raw.githubusercontent.com/purescript/registry/master/new-packages.json
-wget https://raw.githubusercontent.com/purescript/registry/master/bower-packages.json
+wget -q https://raw.githubusercontent.com/purescript/registry/master/new-packages.json
+wget -q https://raw.githubusercontent.com/purescript/registry/master/bower-packages.json
 
 REGISTRY=$(cat bower-packages.json new-packages.json | jq -s 'map(keys) | add | unique | map (sub("purescript-";""))')
 PKGSET=$(cat packages.json | jq -s 'map(keys) | add')
 
 RESULT=$(jq -n --argjson registry "${REGISTRY}" --argjson pkgset "${PKGSET}" '$pkgset-$registry')
 
+rm new-packages.json bower-packages.json
 
 if [ "${RESULT}" == "[]" ]; then
+  echo "[SUCCESS] All packages are in the registry"
   exit 0
 else
   echo "[ERROR] Found some packages not in the registry:"
