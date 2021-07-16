49ae0cba2ad09546b7437371c3f7a2d480a6b559
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2021-07-16 15:48:54 +0200

    Upgrade to purescript 0.14.3 (#972)

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ let
     pkgs.fetchFromGitHub {
       owner = "justinwoo";
       repo = "easy-purescript-nix";
-      rev = "47bdc016c7d56e987ca1aca690b1d6c9816a8584";
-      sha256 = "051fzxd03y0c63sll2bhn0h66dywy9lw6ylyh5vq8fymvix20q94";
+      rev = "bbef4245cd6810ea84e97a47c801947bfec9fadc";
+      sha256 = "00764zbwhbn61jwb5px2syzi2f9djyl8fmbd2p8wma985af54iwx";
     }
   ) {
     inherit pkgs;
